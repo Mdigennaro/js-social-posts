56674560569b59dd955d0ed4ae0aa1bd2a7d56ec
Mdigennaro <michele.digennaro719@gmail.com>
2021-11-05 17:27:37 +0100

    inserimento foto profilo

--- js/main.js
+++ js/main.js
@@ -56,60 +56,111 @@ const posts = [
     }
 ];
 
-console.log(posts);
+
+
 //Richiamo il container dove inserire i post
 const containerPosts = document.getElementById('container');
 
 
 // creo il contenuto del container dove inserire i post
 for (let i = 0; i < posts.length; i++) {
-  //Creo il div dove inserire tutti i dati del singolo post  
-  const post = document.createElement('div');
-  post.classList.add('post');
-  containerPosts.append(post);
 
-  console.log(containerPosts);
+    const postAttuale = posts[i];
+
+    
+    //Creo il div dove inserire tutti i dati del singolo post  
+    const post = document.createElement('div');
+    post.classList.add('post');
+    containerPosts.append(post);
+    
+    console.log(containerPosts);
+    
+    //Creiamo l'header del post
+    const postHeader = document.createElement('div');
+    postHeader.classList.add('post__header');
+    post.append(postHeader);
+    
+    const postMeta = document.createElement('div');
+    postMeta.classList.add('post-meta');
+    postHeader.append(postMeta);
+    
+    //foto profilo
+    const fotoProfilo = document.createElement('div');
+    fotoProfilo.classList.add('post-meta__icon');
+    postMeta.append(fotoProfilo);
+    
+    const media = postAttuale.media;
+    console.log('media', media);
+    
+    fotoProfilo.innerHTML =
+    `
+    <img class="profile-pic" src="${media}" alt="Phil Mangione">
+    `
+    
+    //Creo div contenitore per il nome e per la pubblicazione del post
+    const nomeData = document.createElement('div');
+    nomeData.classList.add('post-meta__data');
+    postMeta.append(nomeData);
   
-  //Creiamo l'header del post
-  const postHeader = document.createElement('div');
-  postHeader.classList.add('post__header');
-  post.append(postHeader);
-
-  const postMeta = document.createElement('div');
-  postMeta.classList.add('post-meta');
-  postHeader.append(postMeta);
-  
-  //foto profilo
-  const fotoProfilo = document.createElement('div');
-  fotoProfilo.classList.add('post-meta__icon');
-  postMeta.append(fotoProfilo);
-
-
-
-
-
-  //Creo div contenitore per il nome e per la pubblicazione del post
-  const nomeData = document.createElement('div');
-  nomeData.classList.add('post-meta__data');
-  postMeta.append(nomeData);
-
   //Nome
   const nomeUtente = document.createElement('div');
   nomeUtente.classList.add('post-meta__author');
   nomeData.append(nomeUtente);
-
-
-
-
+  
+  
+  
+  
 
   //Data pubblicazione
   const dataPost = document.createElement('div');
   dataPost.classList.add('post-meta__time');
   nomeData.append(dataPost)
+  
+  
+  
+  
+  
+  /////////////////////
+  //Creo l'area del testo dell'utente
+  const testoPost = document.createElement('div');
+  testoPost.classList.add('post__text');
+  post.append(testoPost);
+
+  
 
   
   
+  //Creo il div dell'immagine del post
+  const immaginePost = document.createElement('div');
+  immaginePost.classList.add('post__image');
+  post.append(immaginePost);
   
   
-  //
+  
+
+  
+  ////////////////////////////////
+  //creo il footer del post
+  const postFooter = document.createElement('div');
+  postFooter.classList.add('post__footer');
+  post.append(postFooter);
+
+  //creo il container dei like
+  const barraLike = document.createElement('div');
+  barraLike.classList.add('likes');
+  postFooter.append(barraLike);
+
+  //Creo il div del bottone dei like
+  const like = document.createElement('div');
+  like.classList.add('likes__cta');
+  barraLike.append(like);
+  
+  //Creo il btn
+  const bottoneLike = document.createElement('a');
+  bottoneLike.classList.add('like-button');
+  like.append(bottoneLike)
+
+  
 }
+
+
